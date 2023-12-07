81ab1028ae6fab46f440a4d8f1c6450becf5a6b1
Joshua Natividad Galindo <natividadgalindoj@roverkids.org>
2023-12-07 13:20:31 -0500

    Challenge 2 final product

---
+++ Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float spawnTimeInterval;
+    float spawnTime = 0.9f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimeInterval += Time.deltaTime;  
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && spawnTimeInterval > spawnTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spawnTimeInterval = 0;
+
+
+        }
+
+
+
+    }
+}
